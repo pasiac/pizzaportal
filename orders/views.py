c14626504e9567d0e946ef9d2682a9ece98435ea
pasiac <dawidpawlowski16@gmail.com>
2019-10-01 15:32:26 +0200

    Add increase/decrease quantity methods

--- orders/views.py
+++ orders/views.py
@@ -24,6 +24,11 @@ def cart(request):
     return render(request, 'orders/cart.html', context)
 
 
+# It would fix code duplication
+def get_order(request, slug):
+    pass
+
+
 # If add to cart button is clicked it add item to cart
 # or if it is already in cart it increase its quantity
 def add_to_cart(request, slug):
@@ -49,10 +54,10 @@ def add_to_cart(request, slug):
     return redirect('orders:cart')
 
 
-# This function remove whole item quantity from cart
+# It will completely delete item from cart
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    # Get user orders
+    # Get users orders
     order_qs = Order.objects.filter(
         user=request.user,
         ordered=False
@@ -81,3 +86,59 @@ def remove_from_cart(request, slug):
         return redirect("orders:menu")
 
 
+# This will decrease quantity of selected item
+# Probably I should create function to get order and ordered_item
+# to avoid code duplication
+def decrease_quantity(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    # Get users orders
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        # Get user cart
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            for item in order.items.all():
+                if item == order_item:
+                    item.quantity -= 1
+            messages.info(request, "Quantity updated.")
+            order.save()
+            return redirect("orders:cart")
+    else:
+        messages.info(request, "You dont have an active order")
+        return redirect("orders:menu")
+
+
+# Its increase quantity also have to fix code duplication
+def increase_quantity(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    # Get users orders
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        # Get user cart
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            for item in order.items.all():
+                if item == order_item:
+                    item.quantity += 1
+            messages.info(request, "Quantity updated")
+            order.save()
+            return redirect("orders:cart")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("orders:menu")
